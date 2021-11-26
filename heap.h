2d809c9d405d86b4580d7fd8833ab8214a04ce91
amithgopi <amith314@gmail.com>
2021-11-26 17:59:54 -0600

    Removed unused files

--- heap.h
+++ heap.h
@@ -5,6 +5,11 @@
 
 using namespace std;
 
+/**
+ * @brief Parent class for heaps used
+ * 
+ * @tparam T type of data to be sored in the heap
+ */
 template <class T>
 class Heap {
     public:
@@ -17,6 +22,12 @@ class Heap {
         virtual void heapify(int i) = 0;
         virtual int getHeapNodeValue(int i) = 0;
 
+        /**
+         * @brief Swap two entries in the heap
+         * 
+         * @param a pointer to first element
+         * @param b pointer to second element
+         */
         void swap(T *a, T *b) {
             T temp = *a;
             *a = *b;
@@ -28,6 +39,10 @@ class Heap {
             size = 0;
         }
 
+        /**
+         * @brief Build a heap from the current elements in the array arr
+         * 
+         */
         void buildHeap() {
             int start = (size/2) - 1;
             for(int i =  start; i >= 0; i--) {
@@ -35,6 +50,11 @@ class Heap {
             }
         }
 
+        /**
+         * @brief Print the values of each element in the heap array. This prints out the vales of an element which gets
+         * compared on heapify operations.
+         * 
+         */
         void printHeap() {
             for(int i=0; i<size; i++) {
                 cout<<getHeapNodeValue(i)<<", ";
@@ -42,6 +62,10 @@ class Heap {
             cout<<endl;
         }
 
+        /**
+         * @brief Sorts the elements in the heap array based on the implmentation of heapify
+         * 
+         */
         void heapSort() {
             int actualSize = size;
             for(int i=0; i<actualSize; i++){
